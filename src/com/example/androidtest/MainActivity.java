b8ea54b0d77d1dfd080f3a89657f54e3bbc2bb1f
suxinmin <suxinmin@androidesk.com>
2013-11-01 15:45:29 +0800

    第三次提交

--- src/com/example/androidtest/MainActivity.java
+++ src/com/example/androidtest/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		System.out.println("------------------------------");
+		System.out.println("-----------------第二次修改-------------");
 	}
 
 	@Override
